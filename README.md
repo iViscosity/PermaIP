ccbc12c3945a0068eebfff1cbe8fcfa534f82d87
Spencer Murphy <iViscositygaming@gmail.com>
2020-05-22 14:10:31 -0400

    PermaIP

---
+++ README.md
@@ -0,0 +1,13 @@
+# Perma IP
+A small ULib module that automatically detects and bans people who attempt to circumvent permanent bans by creating new accounts. This is similar to Family Share blocking modules, such as [this one by Timmy](https://forums.ulyssesmod.net/index.php/topic,9087.0.html), but it doesn't actually check Family Sharing, just their IPs.
+
+# Dependencies
+[ULib](https://www.github.com/TeamUlysses/ULib)
+
+# Installation
+
+## Workshop
+To install on the Workshop, simply add [this](https://steamcommunity.com/sharedfiles/filedetails/?id=2105319244) to your server's workshop collection. For more information, [the Garry's Mod wiki](https://wiki.facepunch.com/gmod/Workshop_for_Dedicated_Servers) has a good tutorial on using the Steam Workshop for Dedicated Servers.
+
+## Classic
+Visit the [Releases page](https://github.com/iViscosity/PermaIP), and download the latest release. **DO NOT** download a ZIP directly from the repository, as it may contain experimental, untested code that can cause issues.
---
+++ lua/ulib/modules/server/permaip.lua
@@ -0,0 +1,38 @@
+--[[
+	Please do not change anything in this file.
+]]--
+
+local ip_cache = {}
+
+local function LogIP(ip)
+	ip_cache[ip] = true
+	sql.Query("INSERT INTO ulib_ip_bans (ip) VALUES ('" .. ip .."')")
+end
+
+local function BanIP(ply, ip)
+	LogIP(ip)
+	ULib.ban(ply, 0, "Attempt to circumvent permanent ban")
+end
+
+-- can't use ULibPlayerBanned here because it only provides their SteamID, and they're kicked before it's called so we can't access player.GetBySteamID
+local function OnPlayerBanned(ply)
+	if not ULib.bans[ply:SteamID()] or ULib.bans[ply:SteamID()].unban ~= 0 then return end
+	local ip = ply:IPAddress():sub(1, -7)
+	LogIP(ip)
+end
+hook.Add("PlayerDisconnected", "OnPlayerBanned", OnPlayerBanned)
+
+local function IsPlayerBanned(ply)
+	local ip = ply:IPAddress():sub(1, -7) -- strip port from IP
+
+	local result = sql.QueryRow("SELECT * FROM ulib_ip_bans WHERE ip = '" .. ip .. "'")
+
+	if (result or ip_cache[ip]) and not ULib.bans[ply:SteamID()] then
+		BanIP(ply, ip)
+	end
+end
+hook.Add("PlayerInitialSpawn", "CheckPlayerBanned_InitialSpawn", IsPlayerBanned)
+
+if not sql.TableExists("ulib_ip_bans") then
+	sql.Query("CREATE TABLE ulib_ip_bans (ip TEXT NOT NULL PRIMARY KEY, name TEXT)")
+end
